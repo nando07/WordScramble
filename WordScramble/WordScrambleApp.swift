ecd0c3fa81d3ab8047dacf07d3eac8c1efb50470
Fernando Gomez <redchameleonapps@gmail.com>
2023-01-19 13:58:39 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Fernando Gomez on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
